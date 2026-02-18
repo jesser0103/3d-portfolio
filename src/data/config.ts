9e293273396061009b345bdc1e571746e0e69966
Jvsser <jasser.souissi.741@gmail.com>
2026-02-18 21:35:15 +0100

    Initial commit

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,12 +1,12 @@
 const config = {
-  title: "Naresh Khatri | Full-Stack Developer",
+  title: "Jasser Souissi | Full-Stack Developer",
   description: {
-    long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
+    long: "Explore the portfolio of Jasser, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
-      "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
+      "Discover the portfolio of Jasser, a full-stack developer creating interactive web experiences and innovative projects.",
   },
   keywords: [
-    "Naresh",
+    "Jasser",
     "portfolio",
     "full-stack developer",
     "creative technologist",
@@ -23,23 +23,23 @@ const config = {
     "Spline",
     "Framer Motion",
   ],
-  author: "Naresh Khatri",
-  email: "naresh.khatri2345@gmail.com",
-  site: "https://nareshkhatri.site",
+  author: "Jasser Souissi",
+  email: "jassersouissi@gmail.com",
+  site: "https://jassersouissi.com",
 
   // for github stars button
-  githubUsername: "naresh-khatri",
+  githubUsername: "jasser-souissi",
   githubRepo: "3d-portfolio",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
   },
   social: {
-    twitter: "https://x.com/nothotchaddi",
-    linkedin: "https://www.linkedin.com/in/naresh-khatri/",
-    instagram: "https://www.instagram.com/hotchaddi",
-    facebook: "https://www.facebook.com/HotChaddi/",
-    github: "https://github.com/Naresh-Khatri",
+    twitter: "https://x.com/jassersouissi",
+    linkedin: "https://www.linkedin.com/in/jasser-souissi/",
+    instagram: "https://www.instagram.com/jassersouissi",
+    facebook: "https://www.facebook.com/jassersouissi/",
+    github: "https://github.com/jasser-souissi",
   },
 };
 export { config };
